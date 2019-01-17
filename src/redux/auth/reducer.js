8e3cee2d641d6354a1be7ae8f60d49cad87a1cf3
QuangHD <quang.hoang@enouvo.com>
2019-01-17 15:45:06 +0700

    Config eslint

--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -2,31 +2,27 @@ import { AuthTypes } from './actions';
 import { makeReducerCreator } from '../reduxCreator';
 
 export const initialState = {
-  isAuthenticated: true,
+  isAuthenticated: false,
   roles: '',
   loginError: false,
-  loginSuccess: false,
+  loginSuccess: false
 };
 
-const loginSuccess = state => {
-  return {
+const loginSuccess = state => ({
     ...state,
     isAuthenticated: true,
     loginError: false,
-    loginSuccess: true,
-  };
-};
+    loginSuccess: true
+  });
 
-const loginFail = (state, action) => {
-  return {
+const loginFail = (state, action) => ({
     ...state,
     isAuthenticated: false,
     loginError: action.error,
-    loginSuccess: false,
-  };
-};
+    loginSuccess: false
+  });
 
 export const auth = makeReducerCreator(initialState, {
   [AuthTypes.LOGIN_AUTH_SUCCESS]: loginSuccess,
-  [AuthTypes.LOGIN_AUTH_FAIL]: loginFail,
+  [AuthTypes.LOGIN_AUTH_FAIL]: loginFail
 });
